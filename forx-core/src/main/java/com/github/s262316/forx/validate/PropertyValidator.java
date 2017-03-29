73b6119d2f063d1a0af32bd7d5317b6d72889208
s262316 <s262316@users.noreply.github.com>
2017-03-29 10:14:00 +0100

    deleted this duplicate classes

--- forx-core/src/main/java/com/github/s262316/forx/validate/PropertyValidator.java
+++
@@ -1,8 +0,0 @@
-package com.github.s262316.forx.validate;
-
-import com.github.s262316.forx.tree.style.Value;
-
-public interface PropertyValidator
-{
-    public abstract boolean validate(Value value);
-}
